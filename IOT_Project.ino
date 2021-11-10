04768c2bd350d33bfa8ec5d1a7b503a676c777fd
Sai Prasad C <sp25102002@gmail.com>
2021-11-10 22:51:42 +0530

    1st Commit

---
+++ IOT_Project.ino
@@ -0,0 +1,73 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 5 //define green LED pin
+#define LED 4 //define red LED
+#define BUZZER 2 //buzzer pin
+int var = 1;
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+Servo myServo; //define servo name
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  myServo.attach(3); //servo pin
+  myServo.write(0); //servo start position
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "07 AC 37 21" && var%2 != 0) //change here the UID of the card/cards that you want to give access
+  {
+    var += 1;
+    Serial.println("Authorized access");
+    Serial.println(LED);
+    Serial.println();
+    digitalWrite(LED, HIGH);
+    delay(500);
+  }
+ 
+ else   {
+    var += 1;
+    Serial.println(" Access denied");
+    Serial.println(LED);
+    Serial.println();
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
+}
