156eebebfbc3f2c7c3404f30602ec75406659df4
Sai Prasad C <sp25102002@gmail.com>
2021-11-12 11:33:57 +0530

    Wrapping up with hardware part

--- IOT_Project.ino
+++ IOT_Project.ino
@@ -32,7 +32,7 @@ char tname[50] = "Mr. Arduino";
 char tsub[50] = "Arduino";
 char s1uid[50] = "57 13 8F C8";
 char s1name[50] = "Student1";
-void us_sensor();
+void getid();
 long time1, time2, dist1, dist2;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
@@ -53,72 +53,6 @@ void setup()
 }
 void loop()
 {
-  
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  //Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content = "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == tuid && tvar % 2 != 0) //change here the UID of the card/cards that you want to give access
-  {
-    tvar += 1;
-    Serial.println("Authorized access");
-    Serial.print("Teacher Name: ");
-    Serial.println(tname);
-    Serial.print("Subject: ");
-    Serial.println(tsub);
-    Serial.println();
-    analogWrite(LED1, 5);
-    delay(500);
-  }
-
-  else if (content.substring(1) == s1uid && s1var % 2 != 0)
-  {
-    s1var += 1;
-    Serial.println("Authorized access");
-    Serial.print("Student Name: ");
-    Serial.println(s1name);
-    Serial.println();
-    analogWrite(LED2, 5);
-    delay(500);
-  }
-  else if (content.substring(1) == tuid  && tvar % 2 == 0)
-  {
-    tvar -= 1;
-    Serial.println(" Exit code: Teacher");
-    Serial.println();
-    digitalWrite(LED1, LOW);
-    delay(500);
-  }
-
-  else if (content.substring(1) == s1uid && s1var % 2 == 0)
-  {
-    s1var -= 1;
-    Serial.println(" Exit Code: Student");
-    Serial.println();
-    digitalWrite(LED2, LOW);
-    delay(500);
-  }
-  
   pinMode(trigpin1, OUTPUT);
   digitalWrite(trigpin1, LOW);
   delayMicroseconds(5);
@@ -127,15 +61,15 @@ void loop()
   delayMicroseconds(10);
 
   digitalWrite(trigpin1, LOW);
-  
+
   pinMode(echopin1, INPUT);
   time1 = pulseIn(echopin1, HIGH);
-  
+
   dist1 = (time1 / 2) * 0.0343;
-  
+  /*
   Serial.print("Distance 1: ");
   Serial.println(dist1);
-
+  */
   pinMode(trigpin2, OUTPUT);
   digitalWrite(trigpin2, LOW);
   delayMicroseconds(5);
@@ -145,14 +79,14 @@ void loop()
 
   digitalWrite(trigpin2, LOW);
 
-  pinMode(echopin2, INPUT);  
+  pinMode(echopin2, INPUT);
   time2 = pulseIn(echopin2, HIGH);
 
   dist2 = (time2 / 2) * 0.0343;
-
+  /*
   Serial.print("Distance 2: ");
   Serial.println(dist2);
-  
+  */
   if (tvar % 2 == 0)
   {
     if (dist1 < 65 && dist1 > 0)
@@ -175,27 +109,80 @@ void loop()
       analogWrite(LED2, 5);
     }
   }
+  // Look for new cards
+    if ( mfrc522.PICC_IsNewCardPresent())
+    {
+      Serial.println("Detected");
+      getid();
+    }
 }
-void us_sensor()
+void getid()
 {
-  pinMode(trigpin1, OUTPUT);
-  digitalWrite(trigpin1, LOW);
-  delayMicroseconds(5);
+  while (1)
+  {
+    
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial())
+    {
+      return;
+    }
+    //Show UID on serial monitor
+    Serial.print("UID tag :");
+    String content = "";
+    byte letter;
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+    Serial.println();
+    Serial.print("Message : ");
+    content.toUpperCase();
+    if (content.substring(1) == tuid && tvar % 2 != 0) //change here the UID of the card/cards that you want to give access
+    {
+      tvar += 1;
+      Serial.println("Authorized access");
+      Serial.print("Teacher Name: ");
+      Serial.println(tname);
+      Serial.print("Subject: ");
+      Serial.println(tsub);
+      Serial.println();
+      analogWrite(LED1, 5);
+      delay(500);
+      break;
+    }
 
-  digitalWrite(trigpin1, HIGH);
-  delayMicroseconds(10);
+    else if (content.substring(1) == s1uid && s1var % 2 != 0)
+    {
+      s1var += 1;
+      Serial.println("Authorized access");
+      Serial.print("Student Name: ");
+      Serial.println(s1name);
+      Serial.println();
+      analogWrite(LED2, 5);
+      delay(500);
+      break;
+    }
+    else if (content.substring(1) == tuid  && tvar % 2 == 0)
+    {
+      tvar -= 1;
+      Serial.println(" Exit code: Teacher");
+      Serial.println();
+      digitalWrite(LED1, LOW);
+      delay(500);
+      break;
+    }
 
-  digitalWrite(trigpin1, LOW);
-
-  pinMode(trigpin2, OUTPUT);
-  digitalWrite(trigpin2, LOW);
-  delayMicroseconds(5);
-
-  digitalWrite(trigpin2, HIGH);
-  delayMicroseconds(10);
-
-  digitalWrite(trigpin2, LOW);
-
-  pinMode(echopin1, INPUT);
-  pinMode(echopin2, INPUT);
+    else if (content.substring(1) == s1uid && s1var % 2 == 0)
+    {
+      s1var -= 1;
+      Serial.println(" Exit Code: Student");
+      Serial.println();
+      digitalWrite(LED2, LOW);
+      delay(500);
+      break;
+    }
+  }
 }
