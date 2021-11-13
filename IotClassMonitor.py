6b344311320575824c6108aaa21940296c0c31a0
Sai Prasad C <sp25102002@gmail.com>
2021-11-13 16:34:17 +0530

    Operating arduino with python2

--- IotClassMonitor.py
+++ IotClassMonitor.py
@@ -18,8 +18,10 @@ while True:
     row1 = cur.fetchall()
     cur.execute("select * from students")
     row2 = cur.fetchall()
+    new = 0
     for i in row1:
         if serin in i:
+            new = 1
             ser.write(b"t")
             if tvar %2 !=0:
                 print("Entry!","\nUid:",i[0],"\nTeacher Name:",i[1],"\nSubject:",i[2])
@@ -30,6 +32,7 @@ while True:
             break
     for j in row2:
         if serin in j:
+            new = 1
             ser.write(b"s")
             if svar %2 !=0:
                 print("Entry!","\nUid:",j[0],"\nStudent Name:",j[1])
@@ -38,3 +41,30 @@ while True:
                 print("Exit!","\nUid:",j[0],"\nStudent Name:",j[1])
                 svar -= 1
             break
+    if new == 0:
+        print("Unregistered ID Detected!")
+        ch = input("Do you want to register this ID?[y or n]")
+        if ch == "y":
+            intype = input("Enter card type:\n1)Teacher\n2)Student\nEnter 1 or 2:")
+            if intype == "1":
+                uid = serin
+                name = input("Enter teacher name:")
+                sub = input("Enter subject:")
+                insert = "insert into teachers(uid,name,sub) values(%s,%s,%s)"
+                value = [(uid ,name ,sub )]
+                cur.executemany(insert, value)
+                print("Sucessfully registered")
+            elif intype == "2":
+                id = serin
+                name = input("Enter Student name:")
+                
+                insert = "insert into students(uid,name) values(%s,%s)"
+                value = [(uid ,name)]
+                cur.executemany(insert, value)
+                print("Sucessfully registered")
+            else:
+                print("invalid entry")
+        elif ch == "n":
+            pass
+        else:
+            print("invalid choice")
